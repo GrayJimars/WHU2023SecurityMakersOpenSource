b3916e8e29911f17b00ebf04f6304158bdc9b976
GrayJimars <lee15970281951@gmail.com>
2023-06-22 16:53:35 +0800

    蒜式字符串

--- debug 练习/README.md
+++ debug 练习/README.md
@@ -1,2 +1,4 @@
  修改请写下来：  
- GrayJimars 添加 打印平行四边形.c、蒜头君的进制难题2.c、地心引力.c
+ GrayJimars 添加 打印平行四边形.c、蒜头君的进制难题2.c、地心引力.c  
+ GrayJimars 添加 蒜式字符串.c  
+ 
---
+++ debug 练习/蒜式字符串.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#define maxn 100010
+char s[maxn];
+int main() {
+    scanf("%s", s);
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        int x;
+        if (s[i] - 'j' >= 0) {
+            x = s[i]-'j';
+        } else {
+            x = 26 - (int)('j' - s[i]);
+        }
+        char y = 'a' + ((int)('s' - 'a') + x) % 26;
+        char z = 'a' + ((int)('k' - 'a') + x) % 26;
+        if (s[i + 1] == y && s[i + 2] == z) {
+            puts("YES");
+            return 0;
+        }
+    }
+    puts("NO");
+    return 0;
+}
