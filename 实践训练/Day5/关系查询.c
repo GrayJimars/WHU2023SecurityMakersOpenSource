ea076c1ac363acb408043cc403b209e616eb0902
GrayJimars <lee15970281951@gmail.com>
2023-06-24 15:58:05 +0800

    UPDATE

--- 实践训练/Day5/关系查询.c
+++ 实践训练/Day5/关系查询.c
@@ -1,39 +1,85 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-typedef struct{
-    char p1[21];
-    char p2[21];
-}Relation;
+#define MAX 1000
 
-int main(){
-    Relation relation[100];
-    int n, m;
+typedef struct {
+    char name[50];
+    int index;
+} Mapping;
+
+bool relation[MAX][MAX];
+
+int main() {
+    int n;
     scanf("%d", &n);
-    
-    for(int i = 0;i < n;i++){
-        scanf("%s %s", relation[i].p1, relation[i].p2);
-    }
-    
-    scanf("%d", &m);
-    
-    char p1[21], p2[21];
-    int find;
-    for(int i = 0;i < m;i++){
-        find = 0;
-        scanf("%s %s", p1, p2);
-        for(int j = 0;j < n;j++){
-            if((!strcmp(relation[j].p1, p1) 
-                && !strcmp(relation[j].p2, p2)) 
-               || (!strcmp(relation[j].p1, p2) 
-                   && !strcmp(relation[j].p2, p1))){
-                find = 1;
+
+    int eid = 0;
+    Mapping m[MAX];
+    memset(relation, false, sizeof(relation));
+
+    while (n--) {
+        char s1[50], s2[50];
+        scanf("%s %s", s1, s2);
+
+        int index1 = -1, index2 = -1;
+        for (int i = 0; i < eid; i++) {
+            if (strcmp(s1, m[i].name) == 0) {
+                index1 = i;
+            }
+            if (strcmp(s2, m[i].name) == 0) {
+                index2 = i;
             }
         }
-        if(find){
+
+        if (index1 == -1 && index2 == -1) {
+            strcpy(m[eid].name, s1);
+            m[eid].index = eid;
+            index1 = eid++;
+
+            strcpy(m[eid].name, s2);
+            m[eid].index = eid;
+            index2 = eid++;
+        } else if (index1 == -1) {
+            strcpy(m[eid].name, s1);
+            m[eid].index = eid;
+            index1 = eid++;
+        } else if (index2 == -1) {
+            strcpy(m[eid].name, s2);
+            m[eid].index = eid;
+            index2 = eid++;
+        }
+
+        relation[index1][index2] = true;
+        relation[index2][index1] = true;
+    }
+
+    int q;
+    scanf("%d", &q);
+
+    while (q--) {
+        char s1[50], s2[50];
+        scanf("%s %s", s1, s2);
+
+        int index1 = -1, index2 = -1;
+        for (int i = 0; i < eid; i++) {
+            if (strcmp(s1, m[i].name) == 0) {
+                index1 = i;
+            }
+            if (strcmp(s2, m[i].name) == 0) {
+                index2 = i;
+            }
+        }
+
+        if (index1 == -1 || index2 == -1) {
+            printf("No\n");
+        } else if (relation[index1][index2] && relation[index2][index1]) {
             printf("Yes\n");
-        }else{
+        } else {
             printf("No\n");
         }
     }
+
     return 0;
 }
