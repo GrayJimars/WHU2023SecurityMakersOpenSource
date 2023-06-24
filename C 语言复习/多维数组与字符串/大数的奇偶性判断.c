3ff8bf7171e6ee1aff2dab7ad632e19a059cee9e
wenxuansong <wenxuansong70@gmail.com>
2023-06-24 19:50:57 +0800

    update

--- C 语言复习/多维数组与字符串/大数的奇偶性判断.c
+++ C 语言复习/多维数组与字符串/大数的奇偶性判断.c
@@ -1,23 +1,31 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #define MAXSIZE 10010
-int main () {
-	char s[MAXSIZE]= {0};
-	gets(s);int f;
-	int flag=0;
-	for(int i=0; i<MAXSIZE; i++) {
-		if(s[i+1]==0) {
-			f=i;
-			break;
-		}
-	}
-	if((s[f]-48)%2==0) {
-		flag=1;
-	}
-	//puts(s);
-	if(flag) {
-		printf("YES");
-	} else {
-		printf("NO");
-	}
+int main()
+{
+    char s[MAXSIZE] = {0};
+    gets(s);
+    int f;
+    int flag = 0;
+    for (int i = 0; i < MAXSIZE; i++)
+    {
+        if (s[i + 1] == 0)
+        {
+            f = i;
+            break;
+        }
+    }
+    if ((s[f] - 48) % 2 == 0)
+    {
+        flag = 1;
+    }
+    // puts(s);
+    if (flag)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
 }
