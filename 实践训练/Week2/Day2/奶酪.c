bdb98ecad93abf772f9e3243635851377a5a4bd2
GrayJimars <lee15970281951@gmail.com>
2023-06-27 10:56:48 +0800

    重构

--- 实践训练/Week2/Day2/奶酪.c
+++ 实践训练/Week2/Day2/奶酪.c
@@ -1,147 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-//空洞
-typedef struct {
-    long long x;//坐标
-    long long y;
-    long long z;
-}Hole;
-Hole hole[1010];//空洞按编号储存
-//邻接表
-typedef struct arc {
-    int number;//边另一头的空洞编号
-    struct arc* nextarc;
-}Arc;//相连关系
-typedef struct linknode {
-    int visit;
-    int number;//空洞编号
-    struct arc* firstarc;
-}LinkNode;
-
-LinkNode* linklist[1010];
-
-//判断两个空洞是否相通
-int isAccessable(Hole h1, Hole h2, long long r) {
-    //圆心距离小于2r
-    return sqrt((h1.x - h2.x) * (h1.x - h2.x)
-        + (h1.y - h2.y) * (h1.y - h2.y)
-        + (h1.z - h2.z) * (h1.z - h2.z)) <= 2 * r;
-}
-//添加无向边
-void addArc(int a, int b) {
-    //边a到b
-    Arc* newarc = (Arc*)malloc(sizeof(Arc));
-    newarc->number = b;
-    newarc->nextarc = linklist[a]->firstarc;
-    linklist[a]->firstarc = newarc;
-    //边b到a
-    newarc = (Arc*)malloc(sizeof(Arc));
-    newarc->number = a;
-    newarc->nextarc = linklist[b]->firstarc;
-    linklist[b]->firstarc = newarc;
-}
-//分配内存
-void initialize(long long n) {
-    for (int i = 1; i <= n; i++) {
-        linklist[i] = (LinkNode*)malloc(sizeof(LinkNode));
-        linklist[i]->number = i;//赋初值，number代表邻接结点对应的洞编号
-        linklist[i]->visit = 0;
-        linklist[i]->firstarc = NULL;
-    }
-}
-//释放内存
-void FreeArc(Arc* arc) {
-    if (arc == NULL) {
-        return;//递归终点
-    }
-    FreeArc(arc->nextarc);//先递归
-    free(arc);//后释放
-}
-void Free(long long n) {
-    for (int i = 1; i <= n; i++) {
-        FreeArc(linklist[i]->firstarc);//递归释放边元素
-        free(linklist[i]);//释放这个邻接结点
-    }
-}
 //循环队列
-//队中的元素
+//队中的元素：空洞
 typedef struct {
-    LinkNode* hole;
-    int step;
-}ElemType;
+    long long x, y, z;
+    int visit, step;
+}Hole;
+typedef Hole ElemType;
 #define QueueLength 5010
-ElemType queue[5010];
-int front = -1, rear = -1;
+typedef struct {
+    ElemType queue[QueueLength];
+    int front, rear;
+}Queue;
 //入队
-void enqueue(ElemType e) {
-    rear = (rear + 1) % QueueLength;
-    queue[rear] = e;
+void enQueue(Queue *q, ElemType e) {
+    q->rear = (q->rear + 1) % QueueLength;
+    q->queue[q->rear] = e;
 }
 //出队
-void dequeue(ElemType* e) {
-    front = (front + 1) % QueueLength;
-    *e = queue[front];
+void deQueue(Queue* q, ElemType *e) {
+    q->front = (q->front + 1) % QueueLength;
+    *e = q->queue[q->front];
+}
+//判断队空
+int qEmpty(Queue* q) {
+    return q->front == q->rear;
+}
+//判断是否到达终点
+int isEnd(ElemType now, long long h, long long r) {
+    return now.z >= h - r;
+}
+//判断两个洞之间是否相通
+int isAccessable(ElemType h1, ElemType h2, long long r) {
+    return (h1.x - h2.x) * (h1.x - h2.x)
+        + (h1.y - h2.y) * (h1.y - h2.y)
+        + (h1.z - h2.z) * (h1.z - h2.z) <= 4 * r * r;
 }
 //广搜
-//判断是否到达终点
-int isend(ElemType now, long long h, long long r) {
-    return hole[now.hole->number].z >= h - r;
-}
-//bfs
-int bfs(long long h, long long r) {
+int bfs(Queue* q, ElemType hole[], long long n, long long h, long long r) {
     ElemType now;//当前的洞
-    while (front != rear) {
-        dequeue(&now);//从队列中出一个到now
-        if (isend(now, h, r)) {//如果满足终点条件
+    while (q->front != q->rear) {
+        q->front = (q->front + 1) % QueueLength;
+        now = q->queue[q->front];//从队列取出一个到now
+        if (isEnd(now, h, r)) {//如果满足终点条件
             return now.step;//返回这个洞的抵达步数
         }
-        Arc* ap = now.hole->firstarc;//根据邻接表
-        while (ap != NULL) {
-            if (!linklist[ap->number]->visit) {//将相互连通且没访问过的洞入队
-                linklist[ap->number]->visit = 1;
-                ElemType temp;
-                temp.hole = linklist[ap->number];
-                temp.step = now.step + 1;//抵达步数为当前洞的步数+1
-                enqueue(temp);//入队
+        for (int i = 1; i <= n; i++) {
+            if (!hole[i].visit && isAccessable(now, hole[i], r)) {
+                hole[i].visit = 1;
+                hole[i].step = now.step + 1;
+                q->rear = (q->rear + 1) % QueueLength;
+                q->queue[q->rear] = hole[i];//入队
             }
-            ap = ap->nextarc;
         }
     }
     return -1;
 }
-
+//获取洞的信息
+void getHoles(Queue* q, ElemType hole[], long long n, long long r) {
+    for (int i = 1; i <= n; i++) {
+        Hole temp;
+        scanf("%lld %lld %lld", &temp.x, &temp.y, &temp.z);
+        temp.visit = 0;
+        if (temp.z <= r) {//如果可以是起点
+            temp.visit = 1;
+            temp.step = 1;
+            q->rear = (q->rear + 1) % QueueLength;
+            q->queue[q->rear] = temp;//入队
+        }
+        hole[i] = temp;//编号为i
+    }
+}
 int main() {
     long long T, n, h, r;
     scanf("%lld", &T);
     while (T--) {
+        //定义洞数组
+        ElemType hole[5010];
+        //创建队列
+        Queue* q = (Queue*)malloc(sizeof(Queue));
+        q->front = q->rear = -1;
         scanf("%lld %lld %lld", &n, &h, &r);//接受n，h，r
-        initialize(n);//根据n分配内存
         //开始获取所有洞的信息
-        for (int i = 1; i <= n; i++) {
-            Hole temp;
-            scanf("%lld %lld %lld", &temp.x, &temp.y, &temp.z);
-            if (temp.z <= r) {//如果可以是起点
-                ElemType e;
-                e.hole = linklist[i];
-                e.step = 1;
-                enqueue(e);//就入队
-            }
-            hole[i] = temp;
-        }
-        //获取洞信息结束
-        //开始构造邻接表
-        for (int i = 1; i <= n; i++) {//遍历1-n号洞
-            for (int j = i + 1; j <= n; j++) {//遍历这个洞之后的洞
-            //因为是无向关系，所以每任意两个洞之间只需要判断一次
-                if (isAccessable(hole[i], hole[j], r)) {//如果连通
-                    addArc(i, j);//加邻接关系（边）
-                }
-            }
-        }
-        printf("%d\n", bfs(h, r));//开始广搜
-        front = rear;//重置队列
-        Free(n);//释放内存
+        getHoles(q, hole, n, r);
+        //开始广搜
+        printf("%d\n", bfs(q, hole, n, h, r));
+        //释放队列
+        free(q);
     }
     return 0;
 }
